196b21055894874dc6219676737cf6a97c52e1b2
yuliatrianandi <yuliatrianandi@gmail.com>
2019-12-04 15:19:03 +0700

    Updates

---
+++ precache-manifest.fe407d788162052d96bc15401517800b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bde12b8e899077954ad9a03f3e663e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4eac7ab09615a99a7e1c",
+    "url": "/monsters-rolodex/static/css/main.3317cf39.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "4eac7ab09615a99a7e1c",
+    "url": "/monsters-rolodex/static/js/main.b064e252.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
